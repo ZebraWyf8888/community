607c34037b0f978709314cee0a1b5d62d04258ae
Wyf <453907683@qq.com>
2019-11-30 17:43:19 +0800

    init rp

---
+++ src/main/java/com/wyfcommunity/WyfcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.wyfcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyfcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyfcommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyfcommunity/WyfcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyfcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyfcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
